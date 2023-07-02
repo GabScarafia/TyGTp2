155a5f70179801445c690766fbf5903ee319ae71
Augusto <augus611@gmail.com>
2023-07-02 16:33:30 -0300

    Agregué resultados de búsqueda También comenté la parte que consulta la API para lo de vegano y vegetariano así no la llamamos mil veces y nos consume todo el crédito mientras probamos, después hay que descomentarlo. Achiqué el example.json aunque ni lo usamos por el maldito CORS pero bueno ya está.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const spoonacularKey = "c133c754558c4409918494b340a64248"
+var spoonacularKey = "c133c754558c4409918494b340a64248"
 const strapiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjg4MjUzMzQwLCJleHAiOjE2OTA4NDUzNDB9.oERsdIaePc2LU-c0Yv14Nr9ZZaVFjWbAnCjtGqaYWgY"
 function selectApiOperation(apiItem){
     hideSections()
@@ -36,6 +36,11 @@ async function searchRecipesByQuery(){
     .catch(error => {
         console.error('Error al hacer la solicitud HTTP:', error);
     });
+    if (data.code == 402) {
+        spoonacularKey = "b3d8248260a64af4aa63ffb4b7c2b2e3"
+        searchRecipesByQuery()
+        return
+    }
     for (let i = 0; i < data.results.length; i++){
         var recipe = data.results[i]
         const newData = await getRecipeInformation(recipe.id)
@@ -44,21 +49,45 @@ async function searchRecipesByQuery(){
     } 
     const lRecipes = data.results.map(item => new G5Recipe(item));
     console.log(lRecipes);
+    const searchSection = document.getElementById("search-section")
+    if (lRecipes.length == 0) {
+        const message = document.createElement("h3")
+        const messageText = document.createTextNode("No se encontraron resultados :(")
+        message.appendChild(messageText)
+        searchSection.appendChild(message)
+        return
+    }
+    for (let i = 0; i < lRecipes.length; i++){
+        console.log(lRecipes[i])
+        const title = document.createElement("h3")
+        const titleText = document.createTextNode(lRecipes[i].title)
+        title.appendChild(titleText)
+        searchSection.appendChild(title)
+    }
 }
 
 async function getRecipeInformation(recipeID){
     /* NOTA PARA EL FUTURO: esta funcion tarda una banda porque tiene que esperar todos los datos, 
     asi que conviene buscar los datos despues de que el usuario decide guardar la receta. Por ahora
     lo dejo asi para no complicarme pero bueno vemos */
-    var data = await fetch('https://api.spoonacular.com/recipes/'+ recipeID +'/information?apiKey='+ spoonacularKey)
+    /* var data = await fetch('https://api.spoonacular.com/recipes/'+ recipeID +'/information?apiKey='+ spoonacularKey)
     .then(response => response.json())
     .catch(error => {
         console.error('Error al hacer la solicitud HTTP:', error);
     });
+        if (data.code == 402) {
+        spoonacularKey = "b3d8248260a64af4aa63ffb4b7c2b2e3"
+        newData = getRecipeInformation(recipeID)
+        return newData
+    }
     var newData = new Object()
     newData.vegetarian = data.vegetarian
-    newData.vegan = data.vegan
+    newData.vegan = data.vegan */
+    // COMENTO TODO PARA NO LLAMAR A LA API MIL VECES MIENTRAS PROBAMOS
+    var newData = new Object()
+    newData.vegetarian = true
+    newData.vegan = false
     return newData
 }
 
-//apiKey=c133c754558c4409918494b340a64248 para probar
+//apiKey=c133c754558c4409918494b340a64248 apiKey=b3d8248260a64af4aa63ffb4b7c2b2e3
